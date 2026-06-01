d159965b2903e409541d6cd472be36da72474fa1
Takuma Isono <Takuma.Isono@sony.com>
2026-06-01 10:57:24 +0900

    ローカル依存ファイルを.gitconfig.localに移動

--- setup.sh
+++ setup.sh
@@ -13,25 +13,24 @@ sudo apt install -y \
     fzf \
     stow
 
+echo "=== set locale ==="
 sudo locale-gen ja_JP.UTF-8
 sudo update-locale LANG=ja_JP.UTF-8
 
-echo "=== install dotfiles ==="
 
 DOTFILES_DIR="$HOME/ghq/github.com/katsuwo20/mydotfiles"
 cd "$DOTFILES_DIR"
 
+echo " === zsh settings ==="
 echo "Applying zsh settings"
 stow -v -t ~ zsh
 
+echo "=== change default shell ==="
+chsh -s $(which zsh)
+
+
 echo "Applying git settings"
 stow -v -t ~ git
-
-
-echo "=== change defautl shell ==="
-
-
-chsh -s $(which zsh)
 
 echo "=== Git Local setting ==="
 LOCAL_GITCONFIG="$HOME/.gitconfig.local"
@@ -48,6 +47,16 @@ else
     echo "git local config already exists"
 fi
 
+echo "=== setup ghq ==="
+mkdir -p "$HOME/ghq"
+git config --file "$HOME/.gitconfig.local" --unset-all ghq.root 2>/dev/null || true
+git config --file "$HOME/.gitconfig.local" --add ghq.root "$HOME/ghq"
+echo "ghq root set to $HOME/ghq"
+
+echo "=== setup git safe.directory === "
+git config --file "$HOME/.gitconfig.local" --unset-all safe.directory 2>/dev/null || true
+git config --file "$HOME/.gitconfig.local" --add safe.directory "$HOME/ghq/*"
+echo "safe.directory set to $HOME/ghq"
 
 
 echo "=== done ==="
