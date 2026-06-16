47ff1f65c8e5224baab8a908fd7ebd05a3ae69a9
Takuma Isono <Takuma.Isono@sony.com>
2026-06-16 10:24:28 +0900

    autopairsの設定を追加
    文字列、コメントアウト上ではautopairs無効
    改行時にペア維持

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,1 +1,27 @@
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup({
+	check_ts = true,
+	-- String/comment node上では自動ペアを抑止
+	ts_config = {
+		lua = { "string", "comment" },
+		javascript = { "string", "template_string", "comment" },
+		typescript = { "string", "template_string", "comment" },
+		jsx = { "string", "comment" },
+		tsx = { "string", "comment" },
+		python = { "string", "comment" },
+		go = { "string", "comment" },
+		rust = { "string", "comment" },
+	},
+
+	-- VSCode寄せ: Enter時のペア改行は維持、Backspaceペア削除は無効
+	map_cr = true,
+	map_bs = false,
+	map_c_h = false,
+	map_c_w = false,
+
+	-- 実運用での誤爆を減らす
+	disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input", "vim" },
+	disable_in_macro = true,
+	disable_in_replace_mode = true,
+	enable_check_bracket_line = true,
+	break_undo = true,
+})
