f350b011a3a1747f7b90c95e08d9923d605228c2
Takuma Isono <Takuma.Isono@sony.com>
2026-06-17 16:30:06 +0900

    treesitterを最新版にupdate

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,41 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"c",
-		"cpp",
-		"bash",
-		"css",
-		"go",
-		"html",
-		"javascript",
-		"json",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"python",
-		"query",
-		"rust",
-		"toml",
-		"tsx",
-		"typescript",
-		"vim",
-		"vimdoc",
-		"yaml",
+-- nvim-treesitter (main branch, Neovim 0.12+)
+require("nvim-treesitter").setup({
+	install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+-- パーサーのインストール (非同期)
+require("nvim-treesitter").install({
+	"c",
+	"cpp",
+	"bash",
+	"css",
+	"go",
+	"html",
+	"javascript",
+	"json",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"query",
+	"rust",
+	"toml",
+	"tsx",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+})
+
+-- ハイライト・インデントを有効化
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"c", "cpp", "bash", "sh", "css", "go", "html", "javascript", "json",
+		"lua", "markdown", "python", "query", "rust", "toml", "tsx",
+		"typescript", "vim", "vimdoc", "yaml",
 	},
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
 })
