ac4ef65a54da85b3a6f03c5eddf264233286438c
Takuma Isono <Takuma.Isono@sony.com>
2026-06-25 22:10:11 +0900

    lua版のcopilotをインストール

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -2,6 +2,7 @@ local cmp = require("cmp")
 
 cmp.setup({
     sources = {
+        { name = "copilot"},
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "buffer" },
@@ -18,4 +19,21 @@ cmp.setup({
 
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     }),
+
+
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            require("copilot_cmp.comparators").prioritize,
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
+    },
+
 })
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,18 +1,11 @@
-vim.g.copilot_no_tab_map = true
-
-vim.keymap.set("i", "<C-l>", 'copilot#Accept("<CR>")', {
-  expr = true,
-  replace_keycodes = false,
+require("copilot").setup({
+  suggestion = {
+    enabled = false,
+  },
+  panel = {
+    enabled = false,
+  },
 })
 
-vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)")
-vim.keymap.set("i", "<C-[>", "<Plug>(copilot-previous)")
-vim.keymap.set("i", "<C-w>", "<Plug>(copilot-accept-word)")
-vim.keymap.set("i", "<C-j>", "<Plug>(copilot-accept-word)")
+require("copilot_cmp").setup()
 
--- インライン補完を無効化ファイルタイプのリスト
-vim.g.copilot_filetypes = {
-  markdown = false,
-  text = false,
-  log = false,
-}
