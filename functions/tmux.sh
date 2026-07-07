8ac71535175dcd0a0ebae141b85b4a98283ce2b3
Takuma Isono <Takuma.Isono@sony.com>
2026-07-07 18:44:26 +0900

    tmuxのkeybindを別ファイル管理

--- functions/tmux.sh
+++ functions/tmux.sh
@@ -1,5 +1,13 @@
+readonly TAG_tmux="tmux"
+
 setup_tmux() {
+    log "$TAG_tmux" "Setting up tmux configuration."
     cd "$DOTFILES_DIR"
 
+    # tmuxのリンクを削除
+    rm -f ~/.tmux.conf
+    rm -f ~/.config/tmux
+    # tmuxのリンク張り直し
     stow -v -t ~ tmux
+    success "$TAG_tmux" "tmux configuration setup completed."
 }
