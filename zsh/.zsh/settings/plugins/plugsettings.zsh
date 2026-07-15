395e2f4bd1b6298190a8ff0afb6c8978060d3b53
Takuma Isono <Takuma.Isono@sony.com>
2026-07-15 10:39:02 +0900

    keybindを修正

--- zsh/.zsh/settings/plugins/plugsettings.zsh
+++ zsh/.zsh/settings/plugins/plugsettings.zsh
@@ -1,17 +1,19 @@
-# pluguinsの設定を読み込む
+# pluginsを読み込む
+_load_plugs() {
+    local dir="$HOME/.zsh/plugins"
+    source "$dir/fzf-tab/fzf-tab.plugin.zsh"
+    source "$dir/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+
+    source "$dir/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" # 最後に読み込むこと
+}
+
+# pluginsの設定を読み込む
 _load_plug_settings() {
     local dir="$HOME/.zsh/settings/plugins"
 
     source "$dir/fzf-tab.zsh"
     source "$dir/zsh-autosuggestions.zsh"
 }
-
-_load_plugs() {
-    local dir="$HOME/.zsh/plugins"
-    source "$dir/fzf-tab/fzf-tab.plugin.zsh"
-    source "$dir/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-}
-
 
 # source時に実行
 _load_plugs
