7b384ceec0c38b6ef61c4c99bf67e0716624c6a8
Takuma Isono <Takuma.Isono@sony.com>
2026-07-16 14:13:18 +0900

    tmux用のaliasを追加

--- zsh/.zsh/settings/core/aliases.zsh
+++ zsh/.zsh/settings/core/aliases.zsh
@@ -1,6 +1,6 @@
-############################################################
+# ===========================
 # よく使うalias
-############################################################
+# ===========================
 
 alias ll='ls -alF'
 alias la='ls -A'
@@ -16,16 +16,36 @@ alias gd='git diff'
 
 alias p='pwd'
 
+# ===========================
 # lazygit
+# ===========================
 alias lg='lazygit'
 
+# ===========================
 # python系
+# ===========================
 alias python='python3'
 alias pip='pip3'
 
-#neovim
+# ===========================
+# neovim
+# ===========================
 alias v='nvim'
 
+# ===========================
+# tmux系
+# ===========================
+# session 関連
+alias tw='tmux new -As work'    # work   セッションが無かったら作ってattatch
+alias ts='tmux new -As sim'     # sim    セッションが無かったら作ってattatch
+alias tg='tmux new -As git'     # git    セッションが無かったら作ってattatch
+alias tc='tmux new -As vscode'  # vscode セッションが無かったら作ってattatch
+
+alias tls='tmux ls'
+
+
+# ===========================
 # CF系
+# ===========================
 alias bja='bjobs -u all'
 
